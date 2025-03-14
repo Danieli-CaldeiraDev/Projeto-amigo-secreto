47bc5e913912cacb33f7812c657541b503969dbc
Danieli-CaldeiraDev <danieli.caldeirasaraujo@gmail.com>
2025-03-14 19:11:52 -0300

    Projeto amigo secreto completo

--- app.js
+++ app.js
@@ -1,2 +1,46 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let nome = [];
+let amigos = [];
+
+function adicionarAmigo() {
+    
+    let inputAmigo = document.getElementById("amigo");
+    let nomeAmigo = inputAmigo.value.trim();
+
+  
+    if (nomeAmigo === '') { 
+        alert ('Por favor, insira um nome');
+    } else {
+        amigos.push(nomeAmigo);
+        console.log(amigos);
+        inputAmigo.value = ""; 
+        inputAmigo.focus(); 
+        listaAmigos();
+    }    
+}
+
+function listaAmigos(){ 
+    var lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ""
+    for (var i = 0; i < amigos.length; i++) {
+        let checagem = document.createElement('li');
+        checagem.textContent = amigos[i];
+        lista.appendChild(checagem);
+
+    }
+}
+          
+function sortearAmigo() {
+
+    if (amigos.length === 0 ) {
+        alert(`Não é possível realizar o sorteio, por favor, insira um nome`);
+    } else {
+        let nomeEscolhido = parseInt(Math.random() * amigos.length) ;
+        var escolha = amigos[nomeEscolhido];
+        resultado.innerHTML = ( `O nome sorteado foi ${escolha}`);  
+
+     }           
+}
+
+
+    
+
+
